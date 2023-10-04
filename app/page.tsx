0f57a7c516e702ac667f4bf5598cdba937a4515f
dillonzellis <dillonzellis@gmail.com>
2023-10-04 13:07:28 -0400

    starting floral showroom

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import { Container } from "@/components/Container";
+import { ButtonLink } from "@/components/ui/ButtonLink";
 
 export default function Home() {
   return (
     <main className="font-sans">
-      <div className="border-b-gray-600 bg-white py-4 text-center text-lg font-semibold text-gray-950">
+      <div className="border-b-4 border-b-gray-400 bg-white py-4 text-center text-lg font-semibold text-gray-950">
         RLM Affairs
       </div>
       <div>
@@ -15,7 +16,7 @@ export default function Home() {
           loop
         />
       </div>
-      <section className="py-8">
+      <section className="border-b-4 border-b-gray-400 py-8">
         <Container>
           <div className="max-w-prose font-thin">
             <h2 className="pb-2 font-serif text-5xl font-medium">
@@ -37,20 +38,31 @@ export default function Home() {
           </div>
         </Container>
       </section>
-      <section>
-        <div>
-          <div>{/* <Image src="" /> */}</div>
-        </div>
-        <div>
-          <h3>Floral Showroom</h3>
-          <p>
-            Floral Showroom: RLM Affairs is a full-service florist serving North
-            Georgia for 40 years. Our award winning designers offer exquisite
-            attention to detail. Whether it’s a single rose or decor for an
-            entire Gala every bloom is hand picked and sourced eco friendly. Our
-            retail storefront offers modern & unique items keeping everyone in
-            mind
-          </p>
+      <section className="border-b-4 border-b-gray-400">
+        <div className="grid grid-cols-12 items-center">
+          <div className="col-span-8">
+            <Image
+              src="/lanterns-1.jpg"
+              alt="lanterns"
+              width={830}
+              height={665}
+              className="h-[500px] object-cover"
+            />
+          </div>
+          <div className="col-span-4 mx-auto space-y-6 px-8 font-thin">
+            <h3 className="font-serif text-3xl font-semibold">
+              Floral Showroom
+            </h3>
+            <p>
+              Floral Showroom: RLM Affairs is a full-service florist serving
+              North Georgia for 40 years. Our award winning designers offer
+              exquisite attention to detail. Whether it’s a single rose or decor
+              for an entire Gala every bloom is hand picked and sourced eco
+              friendly. Our retail storefront offers modern & unique items
+              keeping everyone in mind
+            </p>
+            <ButtonLink href="/">Discover</ButtonLink>
+          </div>
         </div>
       </section>
     </main>
