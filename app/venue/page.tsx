514a4d1bea8041ee1244a4a57460680839af9661
dillonzellis <dillonzellis@gmail.com>
2023-10-06 16:29:21 -0400

    starting venue page

--- app/venue/page.tsx
+++ app/venue/page.tsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+
 const VenuePage = () => {
   return (
     <main>
-      <h1>Venue Page</h1>
+      <Image
+        src="/venue.jpeg"
+        alt="venue"
+        width={1920}
+        height={600}
+        className="max-h-[600px] object-cover brightness-75"
+      />
+      <section className="py-10">
+        <div className="relative">
+          <div className="absolute left-4 top-1/2 w-[40%] -translate-y-1/2 rounded-full border-t-2 border-t-zinc-400/50"></div>
+          <div className="absolute right-4 top-1/2 w-[40%] -translate-y-1/2 border-t-2 border-t-zinc-400/50"></div>
+          <h1 className="mx-auto pb-4 text-center font-serif text-5xl">
+            Venue
+          </h1>
+        </div>
+        <p className="mx-auto max-w-[90ch] text-center">
+          Pendry Manhattan West is a central spot for casual gatherings, not so
+          casual special celebrations, and everything in-between. Featuring
+          three distinct food & beverage destinations, a central perch in the
+          city’s Manhattan West neighborhood and stunning backdrops from lobby
+          to rooftop, every outing becomes an occasion.
+        </p>
+      </section>
     </main>
   );
 };
