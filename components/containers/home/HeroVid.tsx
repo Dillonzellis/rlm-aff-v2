ba2d0b25cbb3ac3246c7f1634eb1e770579c4a1e
dillonzellis <dillonzellis@gmail.com>
2023-10-09 16:45:01 -0400

    add placeholder youtube vid

--- components/containers/home/HeroVid.tsx
+++ components/containers/home/HeroVid.tsx
@@ -3,19 +3,30 @@ import { ButtonLink } from "@/components/ui/ButtonLink";
 export const HeroVid = () => {
   return (
     <div className="relative">
-      <video
-        src="https://res.cloudinary.com/dwopez4fs/video/upload/v1696387715/yt1s.com_-_Universal_EventSpace_Toronto_Wedding_Venue_Tour_Video_Walkthrough_720p_bpbfvj.mp4"
-        // autoPlay
-        muted
-        loop
-        className=""
-      />
-      <div className="absolute bottom-10 left-10 w-[50%] rounded-sm bg-white/60 px-12 py-6 backdrop-blur">
+      {/* <video */}
+      {/*   src="https://res.cloudinary.com/dwopez4fs/video/upload/v1696387715/yt1s.com_-_Universal_EventSpace_Toronto_Wedding_Venue_Tour_Video_Walkthrough_720p_bpbfvj.mp4" */}
+      {/*   autoPlay */}
+      {/*   muted */}
+      {/*   loop */}
+      {/*   className="" */}
+      {/* /> */}
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/y7GCz-M9Rgw?si=4lCp0khUqVKUglem"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+        className="h-[600px] w-full"
+      ></iframe>
+      <div className="absolute bottom-10 left-20 w-[65%] rounded-sm bg-white/60 px-12 py-4 drop-shadow backdrop-blur">
         <div className="flex items-center justify-between">
           <div className="space-y-2">
-            <div className="text-4xl font-thin">RLM Affairs</div>
+            <div className="text-4xl">Discover RLM Affairs</div>
             <p className="max-w-[35ch] text-sm capitalize italic">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              A venue where dreams meet reality. Experience the magic
+              first-hand.
             </p>
           </div>
           <div>
