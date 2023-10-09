24b63e19ef6ad3c9a17a781f56f25ff006b951ab
dillonzellis <dillonzellis@gmail.com>
2023-10-09 13:38:41 -0400

    make venue FullHeight section

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,20 @@ export default function Home() {
         btnText="Discover"
       />
       <CTA />
-      <NewFeature />
+      <FullHeightImageSection
+        imgSrc="/venue-feature.jpeg"
+        imgAlt=""
+        heading="Venue Feature"
+        body="Floral Showroom: RLM Affairs is a full-service florist serving North
+        Georgia for 40 years. Our award winning designers offer exquisite
+        attention to detail. Whether it’s a single rose or decor for an
+        entire Gala every bloom is hand picked and sourced eco friendly. Our
+        retail storefront offers modern & unique items keeping everyone in
+        mind
+        "
+        btnLink="/"
+        btnText="Discover"
+      />
     </main>
   );
 }
