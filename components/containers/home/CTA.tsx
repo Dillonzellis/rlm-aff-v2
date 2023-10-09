a5b81e01623d4e9b1f2ab5b63a93cd2402a3a433
dillonzellis <dillonzellis@gmail.com>
2023-10-09 12:03:48 -0400

    add cta section

---
+++ components/containers/home/CTA.tsx
@@ -0,0 +1,16 @@
+import { ButtonLink } from "@/components/ui/ButtonLink";
+import Link from "next/link";
+
+export const CTA = () => {
+  return (
+    <section className="py-28">
+      <div className="mx-auto max-w-prose space-y-4">
+        <div className="text-center text-5xl font-thin">Some Heading</div>
+        <p className="text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+          repudiandae cumque quis ipsa nisi cum quasi.
+        </p>
+      </div>
+    </section>
+  );
+};
