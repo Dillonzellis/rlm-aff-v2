902b5027585bbd3188ae76c59d9b204f4c259766
dillonzellis <dillonzellis@gmail.com>
2023-10-09 16:45:10 -0400

    add border to ButtonLink

--- components/ui/ButtonLink.tsx
+++ components/ui/ButtonLink.tsx
@@ -4,7 +4,10 @@ type ButtonLinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
 export const ButtonLink = ({ children, ...props }: ButtonLinkProps) => {
   return (
-    <a className="flex items-center gap-4 font-normal capitalize" {...props}>
+    <a
+      className="flex inline-flex items-center gap-4 rounded-sm border border-zinc-800 px-6 py-4 font-normal capitalize"
+      {...props}
+    >
       {children}
       <span>
         <BsArrowRight className="h-5 w-5" />
