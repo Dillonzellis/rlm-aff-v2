14b63fd7491504ec0e368a7fda9e392b1b8c1206
dillonzellis <dillonzellis@gmail.com>
2023-10-09 16:44:41 -0400

    add content to CTA

--- components/containers/home/CTA.tsx
+++ components/containers/home/CTA.tsx
@@ -1,14 +1,24 @@
 import { ButtonLink } from "@/components/ui/ButtonLink";
+import { Heading } from "@/components/ui/Heading";
 import Link from "next/link";
 
 export const CTA = () => {
   return (
     <section className="py-28">
-      <div className="mx-auto max-w-prose space-y-4">
-        <div className="text-center text-5xl font-thin">Some Heading</div>
-        <p className="text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-          repudiandae cumque quis ipsa nisi cum quasi.
+      <div className="mx-auto max-w-prose">
+        <Heading
+          level="h2"
+          size="medium"
+          weight="thin"
+          className="pb-2 text-center"
+        >
+          Elevate Your Experience
+        </Heading>
+        <hr className="mx-auto max-w-md border-t-2 border-zinc-200 pb-6" />
+        <p className="text-center font-extralight">
+          Discover a realm where intricate design meets natural beauty. Every
+          corner of RLM Affairs is meticulously crafted, ensuring your event is
+          nothing short of extraordinary.
         </p>
       </div>
     </section>
