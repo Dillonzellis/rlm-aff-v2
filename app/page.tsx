9fc4919457b06d08e3de2dd4d6b9356ac3f4c3a9
dillonzellis <dillonzellis@gmail.com>
2023-10-10 16:12:22 -0400

    add event planning section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
 import { HeroVid } from "@/components/containers/home/HeroVid";
 import { Welcome } from "@/components/containers/home/Welcome";
 import { CTA } from "@/components/containers/home/CTA";
 import { FullHeightImageSection } from "@/components/containers/layout/FullHeightImageSection";
+import { Heading } from "@/components/ui/Heading";
+import { Container } from "@/components/Container";
+import { ButtonLink } from "@/components/ui/ButtonLink";
 
 export default function Home() {
   return (
@@ -41,6 +45,31 @@ export default function Home() {
       />
 
       {/* Event Planning */}
+      <section className="py-14">
+        <Container>
+          <div className="grid grid-cols-2 items-center gap-8">
+            <Image
+              src="/entrance.jpeg"
+              alt=""
+              width={740}
+              height={520}
+              className="max-h-[520px] w-full rounded-sm object-cover"
+            />
+            <div className="flex h-full w-full flex-col justify-center space-y-8 bg-[#F2F5F5] px-12">
+              <Heading level="h2" size="large" weight="thin">
+                Event Planning
+              </Heading>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Tenetur officiis amet delectus adipisci aut, iusto assumenda
+                dignissimos ipsa ullam aliquam enim perferendis? Sed quas quod
+                expedita aliquid vitae quia eos!
+              </p>
+              <ButtonLink href="">Learn More</ButtonLink>
+            </div>
+          </div>
+        </Container>
+      </section>
 
       {/* Catering */}
 
