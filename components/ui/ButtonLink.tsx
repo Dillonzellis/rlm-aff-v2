4a17d7d2805b385871fc3905c5a771fbdc481f79
dillonzellis <dillonzellis@gmail.com>
2023-10-09 20:35:19 -0400

    add color to ButtonLink

--- components/ui/ButtonLink.tsx
+++ components/ui/ButtonLink.tsx
@@ -5,7 +5,7 @@ type ButtonLinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement>;
 export const ButtonLink = ({ children, ...props }: ButtonLinkProps) => {
   return (
     <a
-      className="flex inline-flex items-center gap-4 rounded-sm border border-zinc-800 px-6 py-4 font-normal capitalize"
+      className="inline-flex items-center gap-4 rounded-sm bg-[#8B754A] px-6 py-4 font-normal capitalize text-zinc-50"
       {...props}
     >
       {children}
