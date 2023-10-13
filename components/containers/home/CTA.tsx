48d5833701f90935efa62835d5f5b316398745a5
dillonzellis <dillonzellis@gmail.com>
2023-10-13 11:15:11 -0400

    make CTA larger

--- components/containers/home/CTA.tsx
+++ components/containers/home/CTA.tsx
@@ -2,17 +2,17 @@ import { Heading } from "@/components/ui/Heading";
 
 export const CTA = () => {
   return (
-    <section className="py-28">
+    <section className="py-36">
       <div className="mx-auto">
         <Heading
           level="h2"
-          size="medium"
+          size="large"
           weight="thin"
           className="pb-4 text-center"
         >
           Elevate Your Experience
         </Heading>
-        <hr className="mx-auto max-w-[50px] border-t-2 border-zinc-200 pb-4" />
+        <hr className="mx-auto max-w-[80px] border-t-2 border-zinc-200 pb-4" />
         <p className="mx-auto max-w-[50ch] text-center font-extralight leading-8">
           Discover a realm where intricate design meets natural beauty. Every
           corner of RLM Affairs is meticulously crafted, ensuring your event is
--- components/containers/home/HeroVid.tsx
+++ components/containers/home/HeroVid.tsx
@@ -2,7 +2,7 @@ import { Container } from "@/components/Container";
 
 export const HeroVid = () => {
   return (
-    <Container size="wide">
+    <Container>
       <div className="relative">
         {/* <video */}
         {/*   src="https://res.cloudinary.com/dwopez4fs/video/upload/v1696387715/yt1s.com_-_Universal_EventSpace_Toronto_Wedding_Venue_Tour_Video_Walkthrough_720p_bpbfvj.mp4" */}
